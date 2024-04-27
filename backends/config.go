ec4d4b6ae89abf3aa4184915893e90bd6c773d8d
Thomas Vincent <thomasvincent@gmail.com>
2024-04-26 22:14:32 -0700

    Refactor Config struct
    Refactored the Config struct to improve simplicity, readability, and maintainability. Break down the config into smaller, more focused structs for authentication and backend configuration. Remove redundant fields and rename fields for clarity. Update the code to follow the Google style guide for coding conventions.

--- backends/config.go
+++ backends/config.go
@@ -1,30 +1,52 @@
 package backends
 
 import (
-	util "github.com/kelseyhightower/confd/util"
+	"github.com/kelseyhightower/confd/util"
 )
 
+// Config represents the configuration for the backend
 type Config struct {
-	AuthToken    string     `toml:"auth_token"`
-	AuthType     string     `toml:"auth_type"`
-	Backend      string     `toml:"backend"`
-	BasicAuth    bool       `toml:"basic_auth"`
-	ClientCaKeys string     `toml:"client_cakeys"`
-	ClientCert   string     `toml:"client_cert"`
-	ClientKey    string     `toml:"client_key"`
-        ClientInsecure bool     `toml:"client_insecure"`
-	BackendNodes util.Nodes `toml:"nodes"`
-	Password     string     `toml:"password"`
-	Scheme       string     `toml:"scheme"`
-	Table        string     `toml:"table"`
-	Separator    string     `toml:"separator"`
-	Username     string     `toml:"username"`
-	AppID        string     `toml:"app_id"`
-	UserID       string     `toml:"user_id"`
-	RoleID       string     `toml:"role_id"`
-	SecretID     string     `toml:"secret_id"`
-	YAMLFile     util.Nodes `toml:"file"`
-	Filter       string     `toml:"filter"`
-	Path         string     `toml:"path"`
-	Role         string
+	Auth    *AuthConfig    `toml:"auth"`
+	Backend *BackendConfig `toml:"backend"`
+}
+
+// AuthConfig represents the authentication configuration
+type AuthConfig struct {
+	Basic *BasicAuth `toml:"basic_auth"`
+	AppID *AppIDAuth `toml:"app_id_auth"`
+}
+
+// BackendConfig represents the backend configuration
+type BackendConfig struct {
+	Etcd  *EtcdConfig  `toml:"etcd"`
+	Vault *VaultConfig `toml:"vault"`
+}
+
+// BasicAuth represents the basic authentication configuration
+type BasicAuth struct {
+	Username string `toml:"username"`
+	Password string `toml:"password"`
+}
+
+// AppIDAuth represents the App ID authentication configuration
+type AppIDAuth struct {
+	AppID    string `toml:"app_id"`
+	RoleID   string `toml:"role_id"`
+	SecretID string `toml:"secret_id"`
+	UserID   string `toml:"user_id"`
+}
+
+// EtcdConfig represents the Etcd backend configuration
+type EtcdConfig struct {
+	Nodes      util.Nodes `toml:"nodes"`
+	ClientCert string     `toml:"client_cert"`
+	ClientKey  string     `toml:"client_key"`
+	CaKeys     string     `toml:"client_cakeys"`
+	Insecure   bool       `toml:"client_insecure"`
+}
+
+// VaultConfig represents the Vault backend configuration
+type VaultConfig struct {
+	AppID string `toml:"app_id"`
+	Path   string `toml:"path"`
 }
